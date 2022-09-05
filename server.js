cd6611b3991f4ea67f04ccc647511167a3bc01ff
koushik <koushik.ssv@gmail.com>
2022-09-05 10:27:53 +0530

    init2

--- server.js
+++ server.js
@@ -73,9 +73,14 @@ app.use(
 
 app.use("/auth", authRoute);
 
-
-
-
+app.use(
+    cors({
+        // origin: process.env.FRONT,//"http://localhost:3000",
+        origin: "http://localhost:3000",
+        methods: "GET,POST,PUT,DELETE",
+        credentials: true,
+    })
+)
 app.use('/api/v1/auth', routes);
 
 
